a65c38291f9f78274dfe855e37da05b6c0f2fc87
iplux <49649756+ipluxteamx@users.noreply.github.com>
2023-02-10 16:37:19 +0100

    Whoopsies

--- Aqua/Miscellaneous/CompileTime.cs
+++ Aqua/Miscellaneous/CompileTime.cs
@@ -2,6 +2,6 @@ using System;
 namespace Aqua.Miscellaneous { 
     static class Compilation { 
         public static readonly string Date = @"10/02/2023"; 
-        public static readonly string Time = @"16:19:54,55"; 
+        public static readonly string Time = @"16:34:39,58"; 
     } 
 } 
